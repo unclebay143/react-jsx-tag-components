92c5135eed9487a725d4c12861107f2b175145eb
Ijeoma Igboagu <ijeonyi@gmail.com>
2022-10-25 18:51:29 +0100

    Update h3_details.js

--- src/components/tags/h3/h3_details.js
+++ src/components/tags/h3/h3_details.js
@@ -4,11 +4,11 @@ export const h3_details = {
     export const H3 = (props) => {
         const { text, classes, styles, children } = props;
         return (
-				<h3 className={classes} style={styles} {...props}>
-					{children || text || "Title 3"}
-				</h3>
-			);
-		};
+			<h3 className={classes} style={styles} {...props}>
+				{children || text || "Title 3"}
+			</h3>
+		);
+	};
 		`,
 
 	author: {
