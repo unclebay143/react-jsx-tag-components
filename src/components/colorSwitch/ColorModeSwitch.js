66ecd4e0d6b52695454ff3229fd2eaf594b56d92
selemondev <selemondev19@gmail.com>
2022-10-29 12:41:35 +0300

    fix: icon display

--- src/components/colorSwitch/ColorModeSwitch.js
+++ src/components/colorSwitch/ColorModeSwitch.js
@@ -10,8 +10,8 @@ const ColorModeSwitch = () => {
                 color: currentTheme === "light" ? '#000' : '#000',
             }} className='py-2 px-2 rounded-md bg-gray-200 hover:bg-gray-100 transition duration-200 ease-in' onClick={() => changeCurrentTheme(currentTheme === 'light' ? 'dark' : 'light')}>
                 {currentTheme === "light" ? (<>
-                    <MoonIcon className='w-6 h-6' />
-                </>) : (<> <SunIcon className='w-6 h-6' />
+                    <SunIcon className='w-6 h-6' />
+                </>) : (<> <MoonIcon className='w-6 h-6' />
                 </>)}
             </button>
         </div>
